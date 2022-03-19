f159617aaccd99d05fd8d25c9c0e13cd04885637
HyeSunJeong <zaqxswcde88@naver.com>
2022-03-19 15:09:56 +0900

    Gender Select RadioButton

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioButton female;
+    RadioButton male;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        female = (RadioButton) findViewById(R.id.female);
+        male = (RadioButton) findViewById(R.id.male);
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "너는 여자구나!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "너는 남자구나!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
